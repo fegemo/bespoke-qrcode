4e21305ec047d672964b277191340da14c8745c9
Flavio Coutinho <fegemo@gmail.com>
2016-03-04 20:33:27 -0300

    Adds a demo

--- demo/demo.js
+++ demo/demo.js
@@ -1,8 +1,6 @@
 window.deck = bespoke.from('article', [
   bespoke.plugins.keys(),
   bespoke.plugins.touch(),
-  bespoke.plugins.classes(),
   bespoke.themes.cube(),
-  bespoke.plugins.bullets('li'),
   bespoke.plugins.qrcode()
 ]);
